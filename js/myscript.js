8b2607ea38bfc03589a3bd97c2750b464fdd2936
linchinxi <linchinxi@gmail.com>
2021-08-20 16:59:44 +0100

    lkjlk

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+// simple model values
+let n = 1
+let complicated = {id:3, address:'here', array:[4,3,2], signed_in:false}
+
+const fn = (x, y,)=>{return x+y}
+
+n = fn(4,2)
+console.log(n, complicated)
