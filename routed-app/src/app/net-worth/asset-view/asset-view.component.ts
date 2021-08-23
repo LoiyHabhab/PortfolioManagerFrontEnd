9c93371b3dc891f0a1a0532e543eff60acff8ba8
linchinxi <linchinxi@gmail.com>
2021-08-23 18:59:58 +0100

    xfgbsd

--- routed-app/src/app/net-worth/asset-view/asset-view.component.ts
+++ routed-app/src/app/net-worth/asset-view/asset-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-asset-view',
@@ -11,6 +12,8 @@ export class AssetViewComponent implements OnInit {
   @Input() price:number = 0
   @Input() code:string = ''
   count:number = 0
+  // declare a custom event for this component
+  @Output() sortie:EventEmitter<number> = new EventEmitter()
 
   constructor() { }
 
@@ -20,6 +23,8 @@ export class AssetViewComponent implements OnInit {
   buyStock(){
     // increment the count
     this.count += 1
+    // emit our custom event to the parent component
+    this.sortie.emit(this.price) // we can send complex data as an object
   }
   sellStock(){
     // increment the count
