b2f9b4bef6cf30bb91290732a629ce2541bedc64
linchinxi <linchinxi@gmail.com>
2021-08-23 16:04:43 +0100

    ;kjh;

--- routed-app/src/app/app-routing.module.ts
+++ routed-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CashFlowComponent } from './cash-flow/cash-flow.component';
+import { MarketMoversComponent } from './market-movers/market-movers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cash-flow', component:CashFlowComponent},
+  {path:'market-movers', component:MarketMoversComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
