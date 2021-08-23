cf7d87627fe75c16ca0f32030aaa5c9f709cc9c0
linchinxi <linchinxi@gmail.com>
2021-08-23 14:59:29 +0100

    lkjhlkjh

--- routed-app/src/app/app.component.ts
+++ routed-app/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // here we can declare our data models
   title = 'routed-app';
+  today = new Date()
+  user = 'Ada'
 }
