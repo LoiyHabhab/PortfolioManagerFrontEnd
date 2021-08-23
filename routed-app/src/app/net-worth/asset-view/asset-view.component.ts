dd44ca292425c4a871041d72519be3fbf27cafe3
linchinxi <linchinxi@gmail.com>
2021-08-23 16:40:59 +0100

    ;lkj

--- routed-app/src/app/net-worth/asset-view/asset-view.component.ts
+++ routed-app/src/app/net-worth/asset-view/asset-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-asset-view',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asset-view.component.css']
 })
 export class AssetViewComponent implements OnInit {
+  // declare data-models for this component
+  // if we intend to inject values, the data-model MUST be an @Input
+  @Input() price:number = 0
 
   constructor() { }
 
--- routed-app/src/app/net-worth/net-worth.component.ts
+++ routed-app/src/app/net-worth/net-worth.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetWorthComponent implements OnInit {
 
+  // declare model data for this component
+  total = 0
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // we can declare methods of this class here
+  modifyParent(value:number){
+    this.total += value
   }
 
 }
