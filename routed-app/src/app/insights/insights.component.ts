b3581a770546e9e52d58b12486727985f681df39
linchinxi <linchinxi@gmail.com>
2021-08-24 19:16:45 +0100

    using an object for the data model

--- routed-app/src/app/insights/insights.component.ts
+++ routed-app/src/app/insights/insights.component.ts
@@ -10,8 +10,9 @@ export class InsightsComponent implements OnInit {
   // wee need data models for this component
   // reportData:any = {} // this is where our returned data from the API will go
   reportData = {name:'', id:1}
-  category:string = 'user'
-  id:number = 1
+  // category:string = 'user'
+  // id:number = 1
+  paramObj = {category:'user', id:1}
   // we need access to the service
   constructor(private typicodeService:TypicodeService) { }
 
@@ -20,7 +21,8 @@ export class InsightsComponent implements OnInit {
   makeServiceCall(){
     // we call the service method by subscribing to it
     // remember the api call will be async so subscribing responds when it returns
-    this.typicodeService.getApiData({category:this.category, id:this.id})
+    // this.typicodeService.getApiData({category:this.category, id:this.id})
+    this.typicodeService.getApiData(this.paramObj)
       .subscribe( (data:any)=>{
         this.reportData = data
       } )
