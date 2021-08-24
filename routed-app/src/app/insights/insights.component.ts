5e8670d7aef77fb3a6359fa6136c9e1babab2e76
linchinxi <linchinxi@gmail.com>
2021-08-24 19:07:19 +0100

    call service with form params

--- routed-app/src/app/insights/insights.component.ts
+++ routed-app/src/app/insights/insights.component.ts
@@ -10,6 +10,8 @@ export class InsightsComponent implements OnInit {
   // wee need data models for this component
   // reportData:any = {} // this is where our returned data from the API will go
   reportData = {name:'', id:1}
+  category:string = 'user'
+  id:number = 1
   // we need access to the service
   constructor(private typicodeService:TypicodeService) { }
 
@@ -18,8 +20,8 @@ export class InsightsComponent implements OnInit {
   makeServiceCall(){
     // we call the service method by subscribing to it
     // remember the api call will be async so subscribing responds when it returns
-    this.typicodeService.getApiData({category:'users', id:3})
-      .subscribe( (data)=>{
+    this.typicodeService.getApiData({category:this.category, id:this.id})
+      .subscribe( (data:any)=>{
         this.reportData = data
       } )
   }
