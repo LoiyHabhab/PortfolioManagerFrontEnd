a3cbe7b77be080d14eb5ec630faa85efffde26df
msong16 <maysong168@gmail.com>
2021-08-25 17:16:06 +0000

    display json data in table

--- routed-app/src/app/insights/insights.component.ts
+++ routed-app/src/app/insights/insights.component.ts
@@ -15,13 +15,10 @@ import * as moment from 'moment';
 //declare var myconfig:any;
 export class InsightsComponent implements OnInit {
   // wee need data models for this component
-   reportData:any = {} // this is where our returned data from the API will go
+   reportData:any = [] // this is where our returned data from the API will go
+   formatedData: any= [];
    myChart:Chart = new Chart('line',this.reportData);
-  //reportData = {name:'', id:1}
-  // category:string = 'user'
-  // id:number = 1
-  //paramObj = {category:'user', id:1}
-  paramObj = {timePeriod:'date', id:2, currentDate:'2021-08-16'}
+   paramObj = {timePeriod:'date', id:2, currentDate:'2021-08-16'}
   // we need access to the service
   constructor(private typicodeService:TypicodeService, private historicalDataService:HistoricalDataService) {
     Chart.register(...registerables);
@@ -34,17 +31,19 @@ export class InsightsComponent implements OnInit {
   makeServiceCall(){
     // we call the service method by subscribing to it
     // remember the api call will be async so subscribing responds when it returns
-    // this.typicodeService.getApiData({category:this.category, id:this.id})
-    // this.typicodeService.getApiData(this.paramObj)
-    //   .subscribe( (data:any)=>{
-    //     this.reportData = data
-    //   } )
     
     // this.historicalDataService.getAllHistoricalData().subscribe((data:any)=>{
     //  this.reportData = data;
     // })
     this.historicalDataService.getDatabyDate(this.paramObj).subscribe((data:any)=>{
       this.reportData = data;
+     this.formatedData = this.reportData
+      for (var key in data){
+        this.formatedData[key] = data[key];
+        this.formatedData[key].date = moment(data[key].date).format('MM/DD/YYYY');
+      }
+      console.log(this.formatedData);
+      console.log(this.reportData);
       if (this.myChart){this.myChart.destroy();}
       var labeldata = [];
 
@@ -80,7 +79,6 @@ export class InsightsComponent implements OnInit {
          }
         }
       })
-      console.log(chartdata)
      })
     
   }
